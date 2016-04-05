2cacd2b4c49f456e48b419fa5fb5ec6839a51c6a
Your Name <you@example.com>
2016-04-05 14:09:01 +0300

    Initial Commit

---
+++ bmi-calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bmi-calculator
+//
+//  Created by iosdev on 5.4.2016.
+//  Copyright © 2016 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
